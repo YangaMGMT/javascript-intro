237790049708b8e6e79bd03d9830787e3644940c
Bill Schofield <bill@billschofield.net>
2015-06-23 11:57:10 -0500

    Added image to project instead of using external link Removed unused files. Move all concepts to global namespace and simplified structure for new learners.

--- js/src/main.js
+++ js/src/main.js
@@ -1,5 +1,15 @@
-"use strict";
+var counter = document.getElementById('counter');
+var tRex = document.getElementById('moving-t-rex');
 
-var game = game || {};
-var runner = game.runner({document: document});
-runner.go(window);
+var position = 0;
+
+function gameLoop() {
+    counter.innerHTML = position;
+    tRex.style.left = position + "px";
+    tRex.style.top = "300px";
+
+    position = position + 2;
+}
+
+var framesPerSecond = 1000 / 60;
+window.setInterval(gameLoop, framesPerSecond);
